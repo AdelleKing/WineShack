05c55701aa11daa2dae49362909089348be54d04
Adelle King <adelle_xx@yahoo.co.uk>
2024-04-19 16:26:08 +0000

    deploying to heroku

--- wineshack/settings.py
+++ wineshack/settings.py
@@ -29,7 +29,9 @@ SECRET_KEY=os.environ.get("SECRET_KEY")
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ['8000-adelleking-wineshack-et4m7rjbhtv.ws-eu110.gitpod.io', 'wineshack.herokuapp.com', 'localhost']
+ALLOWED_HOSTS = ['8000-adelleking-wineshack-et4m7rjbhtv.ws-eu110.gitpod.io', 
+                    'wineshack.herokuapp.com', 'localhost', 
+                    'wineshack-c948e5d21774.herokuapp.com']
 
 
 # Application definition
